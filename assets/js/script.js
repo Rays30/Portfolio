9cfb4533607e583c45b6bf960b6825cdcc08ea95
Rays30 <sabellanoraysel16@gmail.com>
2026-04-24 14:10:59 +0800

    remove API key from script.js, add config.js to gitignore

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
 import { getFirestore, doc, getDoc, updateDoc, increment } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyAJWpTOo4Wyq3BGx90G1J8rBmZOPUJN-YU",
-    authDomain: "raysel-portfolio.firebaseapp.com",
-    projectId: "raysel-portfolio",
-    storageBucket: "raysel-portfolio.firebasestorage.app",
-    messagingSenderId: "267078492486",
-    appId: "1:267078492486:web:465a045b68b4239c744547",
-    measurementId: "G-N9BCHNFNKM"
-};
+import { firebaseConfig } from "./config.js";
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
